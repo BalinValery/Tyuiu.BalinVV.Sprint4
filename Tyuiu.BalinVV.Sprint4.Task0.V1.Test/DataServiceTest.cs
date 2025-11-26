513799e135514e3fdcc06f6142defc726a6d87a2
BalinValery <valera.balin00@mail.ru>
2025-11-26 23:52:14 +0500

    Add project files.

---
+++ Tyuiu.BalinVV.Sprint4.Task0.V1.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.BalinVV.Sprint4.Task0.V1.Lib;
+namespace Tyuiu.BalinVV.Sprint4.Task0.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckValidGetSumEvenArrEl()
+        {
+            DataService ds = new DataService();
+            int[] Array = new int[] { 6, 4, 3, 2, 1, 0, 9, 8, 7, 5 };
+            int wait = 20;
+            int res = ds.GetSumEvenArrEl(Array);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BalinVV.Sprint4.Task0.V1/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.BalinVV.Sprint4.Task0.V1.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #4 | Выполнил: Балин В.В. | СМАРТб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Одномерный статический массив                                     *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #1                                                              *");
+        Console.WriteLine("* Выполнил: Балин В.В. | СМАРТб-25-1                                      *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан одномерный целочисленный массив на 10 элементов заполненный         *");
+        Console.WriteLine("*    статическими значениями в диапазоне от 0 до 9   подсчитать сумму     *");
+        Console.WriteLine("*четных элементов массива                                                 *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("{6 ,4 ,3 ,2 ,1 ,0 ,9 ,8 ,7 ,5}");
+        int[] Array = new int[] { 6, 4, 3, 2, 1, 0, 9, 8, 7, 5 };
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        int res = ds.GetSumEvenArrEl(Array);
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
