5c28d9855736007881b9bdcec43ba5c9fd6d5b0a
BalinValery <valera.balin00@mail.ru>
2025-11-27 19:33:58 +0500

    Task4

---
+++ Tyuiu.BalinVV.Sprint4.Task4.V30/Program.cs
@@ -0,0 +1,77 @@
+﻿using Tyuiu.BalinVV.Sprint4.Task4.V30.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #4 | Выполнил: Балин В.В. | СМАРТб-25-1";
+        Console.WriteLine("****************************************************************************");
+        Console.WriteLine("* Спринт #4                                                                *");
+        Console.WriteLine("* Тема: Обработка структурных типов                                        *");
+        Console.WriteLine("* Задание #4                                                               *");
+        Console.WriteLine("* Вариант #30                                                              *");
+        Console.WriteLine("* Выполнил: Балин В.В. | СМАРТб-25-1                                        ");
+        Console.WriteLine("****************************************************************************");
+        
+        Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+        Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный         *");
+        Console.WriteLine("* случайными значениями в диапазоне от 3 до 7. Заменить нечетные элементы  *");
+        Console.WriteLine("* массива на 0.                                                            *");
+        Console.WriteLine("* 3, 4, 6, 6, 5,                                                           *");
+        Console.WriteLine("* 6, 7, 6, 3, 5,                                                           *");
+        Console.WriteLine("* 5, 3, 5, 7, 6,                                                           *");
+        Console.WriteLine("* 6, 4, 7, 6, 6,                                                           *");
+        Console.WriteLine("* 7, 4, 3, 4, 5,                                                           *");
+        Console.WriteLine("****************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+        Console.WriteLine("****************************************************************************");
+
+        Console.WriteLine("Введите количество строк массива: ");
+        int rows = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите количество строк массива: ");
+        int columns = Convert.ToInt32(Console.ReadLine());
+
+        int[,] matrix = new int[rows, columns];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.WriteLine($"Введите {i},{j} элемент массива");
+                matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        Console.WriteLine("\nМассив:");
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"{matrix[i, j]} \t");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+
+        Console.WriteLine("****************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+        Console.WriteLine("****************************************************************************");
+
+        int[,] res = ds.Calculate(matrix);
+
+        Console.WriteLine("\nИзмененный массив:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"{res[i, j]} \t");
+            }
+            Console.WriteLine();
+        }
+        Console.ReadKey();
+    }
+}
