49477afd1d751a66e7f86aab7207dacfe2a3b869
BalinValery <valera.balin00@mail.ru>
2025-11-27 21:00:14 +0500

    Task7

---
+++ Tyuiu.BalinVV.Sprint4.Task7.V28.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.BalinVV.Sprint4.Task7.V28.Lib;
+namespace Tyuiu.BalinVV.Sprint4.Task7.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckValidCalculate()
+        {
+            int rows = 5;
+            int columns = 3;
+            int[,] mrex = new int[rows, columns];
+            string str = "623351179845632";
+            DataService ds = new DataService();
+            int res = ds.Calculate(rows, columns, str);
+            int wait = 4608;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
