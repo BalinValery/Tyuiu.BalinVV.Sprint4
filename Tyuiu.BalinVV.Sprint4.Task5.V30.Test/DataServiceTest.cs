285a5df344a9b9e6221c8f652cfbd7399f761bbe
BalinValery <valera.balin00@mail.ru>
2025-11-27 19:46:42 +0500

    Task5

---
+++ Tyuiu.BalinVV.Sprint4.Task5.V30.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.BalinVV.Sprint4.Task5.V30.Lib;
+namespace Tyuiu.BalinVV.Sprint4.Task5.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[,] matrix = new int[,] { {-1,-1,-1,-1,-1 },
+                                         {-1,-1,-1,-1,-1 },
+                                         {-1,-1,-1,-1,-1 },
+                                         {-1,-1,-1,-1,-1 },
+                                         {-1,-1,-1,-1,4 }
+                                            };
+
+            int res = ds.Calculate(matrix);
+            int wait = 4;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
